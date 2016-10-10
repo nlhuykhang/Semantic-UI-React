5062e022fc6a930c56c21350592f4058d0e959b2
Levi Thomason <me@levithomason.com>
2016-10-09 23:03:11 -0700

    wip shorthand collection keys

--- src/modules/Dropdown/Dropdown.js
+++ src/modules/Dropdown/Dropdown.js
@@ -4,7 +4,7 @@ import React, { Children, cloneElement, PropTypes } from 'react'
 
 import {
   AutoControlledComponent as Component,
-  createShorthand,
+  createShorthandItem,
   customPropTypes,
   getElementType,
   getUnhandledProps,
@@ -1020,7 +1020,7 @@ export default class Dropdown extends Component {
 
     return (
       <DropdownMenu className={menuClasses}>
-        {createShorthand(DropdownHeader, val => ({ content: val }), header)}
+        {createShorthandItem(DropdownHeader, val => ({ content: val }), header)}
         {this.renderOptions()}
       </DropdownMenu>
     )
